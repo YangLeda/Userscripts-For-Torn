245c05dae2498a4a1967eb560159d9e14fe9504e
Leda Yang <yangleda@outlook.com>
2024-03-12 03:40:01 +0800

    Update SimCompanies-Torn.js

--- SimCompanies-Torn.js
+++ SimCompanies-Torn.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SimCompanies-Torn
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.4
 // @description  None
 // @author       bot_7420
 // @match        https://www.simcompanies.com/*
@@ -12,7 +12,11 @@
 (function () {
     "use strict";
 
+    // 交易所页面自定义输入价格
+    const CustomExchangeInputPrices = [10, 100, 1000, 10000];
+
     let pageSpecifiedTimersList = [];
+    let notesElement = null;
 
     let lastKnownURL = "";
     const mainCheckingURLLoop = () => {
@@ -54,7 +58,55 @@
             handleWarehouseItem();
         } else if (currentURL.includes("/b/")) {
             handleCustomHourInput();
+        } else if (currentURL.includes("/market/resource/")) {
+            // 交易所 高亮6件最近访问物品
+            handleExchangeHighlightRecent();
+            // 交易所 自定义输入价格
+            handleExchangeCustomInputPrices();
         }
+    }
+
+    function handleExchangeHighlightRecent() {
+        const checkElementExist = () => {
+            const selectedElems = document.querySelectorAll(`a.hover-effect.css-k8l72z`);
+            if (selectedElems.length > 50) {
+                const recentExchangeItemsString = localStorage.getItem("recentlyVisitedExchangeResources");
+                const recentExchangeItems = JSON.parse(recentExchangeItemsString);
+                for (const elem of selectedElems) {
+                    elem.classList.remove("script-highlighted");
+                }
+                let markedNum = 0;
+                for (const itemId of recentExchangeItems) {
+                    if (markedNum < 6) {
+                        markedNum++;
+                        const target = document.querySelector(`div.css-hf83mx a.hover-effect.css-k8l72z[href*="/resource/${itemId}/"]`);
+                        target.classList.add("script-highlighted");
+                    }
+                }
+            }
+        };
+        const tempTimer = setInterval(checkElementExist, 1000);
+        pageSpecifiedTimersList.push(tempTimer);
+    }
+
+    function handleExchangeCustomInputPrices() {
+        const checkElementExist = () => {
+            const selectedElem = document.querySelector(`input.css-1whp23o.form-control[name="quantity"]`);
+            if (selectedElem) {
+                clearInterval(timer);
+                for (const price of CustomExchangeInputPrices) {
+                    let a = document.createElement("a");
+                    let linkText = document.createTextNode(" " + price + " ");
+                    a.style.padding = "10px 5px";
+                    a.appendChild(linkText);
+                    a.onclick = () => {
+                        setInput(selectedElem, price);
+                    };
+                    selectedElem.parentNode.parentNode.parentNode.appendChild(a);
+                }
+            }
+        };
+        let timer = setInterval(checkElementExist, 100);
     }
 
     function setInput(inputNode, value) {
@@ -67,7 +119,6 @@
     }
 
     function handleCustomHourInput() {
-        console.log("SimCompanies-Torn: handleCustomHourInput");
         const checkElementExist = () => {
             const selectedElems = document.querySelectorAll("h3 > svg");
             let isReady = selectedElems.length > 0;
@@ -87,8 +138,6 @@
                     newNode.onclick = (e) => {
                         let target_node = e.target.parentElement.parentElement.querySelector("input");
                         let target_text = e.target.innerText;
-                        console.log(target_node);
-                        console.log(target_text);
                         target_node.click();
                         setInput(target_node, target_text);
                         e.preventDefault();
@@ -104,8 +153,6 @@
                     newNode2.onclick = (e) => {
                         let target_node = e.target.parentElement.parentElement.querySelector("input");
                         let target_text = e.target.innerText;
-                        console.log(target_node);
-                        console.log(target_text);
                         target_node.click();
                         setInput(target_node, target_text);
                         e.preventDefault();
@@ -121,13 +168,11 @@
     }
 
     function handleWarehouseItem() {
-        console.log("SimCompanies-Torn: handleWarehouseItem");
         const checkElementExist = () => {
             const table = document.querySelector(`.css-1vwotq4.e12j7voa6`);
             const input = document.querySelector(`input[name="price"]`);
             if (table && input && !input.classList.contains("script_checked")) {
-                console.log("SimCompanies-Torn: handleWarehouseItem check number");
-                let exchangePrice = Number(table.querySelector("span.css-rnnx2x").nextSibling.nextSibling.textContent);
+                let exchangePrice = Number(table.querySelector("span.css-rnnx2x").nextSibling.nextSibling.textContent.replace(",", ""));
                 let discountedPrice = exchangePrice * 0.97;
                 exchangePrice = exchangePrice.toFixed(3);
                 discountedPrice = discountedPrice.toFixed(3);
@@ -165,7 +210,6 @@
     }
 
     function handleLandscape() {
-        console.log("SimCompanies-Torn: handleLandscape");
         const checkElementExist = () => {
             if (document.getElementsByClassName("test-headquarters").length > 0) {
                 clearInterval(timer);
@@ -185,7 +229,6 @@
     }
 
     function landscape_moveGatheringIcons() {
-        console.log("SimCompanies-Torn: landscape_moveGatheringIcons");
         const icons = document.querySelectorAll("img.css-hqao0z.ejaaut33");
         for (const icon of icons) {
             if (!icon.classList.contains("script_moved_to_top_right")) {
@@ -195,7 +238,6 @@
     }
 
     function landscape_removeRush() {
-        console.log("SimCompanies-Torn: landscape_removeRush");
         const banners = document.querySelectorAll(".link-button.css-lgo4vi");
         for (const elm of banners) {
             elm.style.display = "none";
@@ -306,46 +348,127 @@
             top:6% !important;
             border:1px solid black !important;
         }`);
+
+        // 聊天弹窗
+        GM_addStyle(`
+        .css-1tvnvpv {
+            left: 5px !important;
+            bottom: 5px !important;
+            max-width: 300px !important;
+            width: 100% !important;
+            z-index: 300 !important;
+        }`);
+        GM_addStyle(`
+        .css-lwisbd {
+            height: 43px !important;
+            margin-top: 2px !important;
+        }`);
+        GM_addStyle(`
+        .chat-notifications .chat-notification img.logo {
+            height: 33px !important;
+            width: 33px !important;
+        }`);
+
+        // 高亮
+        GM_addStyle(`
+        .script-highlighted {
+            background-color: #FFC107 !important;
+        }`);
+
+        // 记事本
+        GM_addStyle(`
+        .script-nav-3 {
+            margin-left: 150px;
+            font-size: 18px;
+            position: absolute;
+            top: 50%;
+            -ms-transform: translateY(-50%);
+            transform: translateY(-50%);
+            color: rgb(184,184,184);
+        }`);
+        GM_addStyle(`
+        textarea.script_notes {
+            position:fixed;
+            bottom:1%;
+            right:1%;
+            z-index: 200;
+            font-size: 18px;
+            background-color: #DCDCDC;
+            max-width: 400px;
+            max-height: 500px;
+        }`);
     }
 
     function global_addNavButtons() {
         const checkElementExist = () => {
-            const selectedElems = document.querySelectorAll(".css-145d0e.e1kr4hqh1");
-            if (selectedElems.length > 0) {
+            if (document.querySelector(".css-145d0e.e1kr4hqh1")) {
                 clearInterval(timer);
+                setTimeout(function () {
+                    const selectedElem = document.querySelector(".css-145d0e.e1kr4hqh1");
+                    let a1 = document.createElement("a");
+                    let linkText1 = document.createTextNode("文库");
+                    a1.appendChild(linkText1);
+                    a1.onclick = () => {
+                        let targetButton = document.querySelector(`a[aria-label="文库"]`);
+                        if (!targetButton) {
+                            document.querySelector(`.css-1ljhlhi`).click();
+                            targetButton = document.querySelector(`a[aria-label="文库"]`);
+                        }
+                        targetButton.click();
+                    };
+                    a1.classList.add("script-nav-1");
+                    selectedElem.parentNode.insertBefore(a1, selectedElem.nextSibling);
 
-                let a1 = document.createElement("a");
-                let linkText1 = document.createTextNode("文库");
-                a1.appendChild(linkText1);
-                a1.onclick = () => {
-                    let targetButton = document.querySelector(`a[aria-label="文库"]`);
-                    if (!targetButton) {
-                        document.querySelector(`.css-1ljhlhi`).click();
-                        targetButton = document.querySelector(`a[aria-label="文库"]`);
-                    }
-                    targetButton.click();
-                };
-                a1.classList.add("script-nav-1");
-                selectedElems[0].parentNode.insertBefore(a1, selectedElems[0].nextSibling);
+                    let a2 = document.createElement("a");
+                    let linkText2 = document.createTextNode("百科");
+                    a2.appendChild(linkText2);
+                    a2.onclick = () => {
+                        let targetButton = document.querySelector(`a[aria-label="百科"]`);
+                        if (!targetButton) {
+                            document.querySelector(`.css-1ljhlhi`).click();
+                            targetButton = document.querySelector(`a[aria-label="百科"]`);
+                        }
+                        targetButton.click();
+                    };
+                    a2.classList.add("script-nav-2");
+                    selectedElem.parentNode.insertBefore(a2, selectedElem.nextSibling);
 
-                let a2 = document.createElement("a");
-                let linkText2 = document.createTextNode("百科");
-                a2.appendChild(linkText2);
-                a2.onclick = () => {
-                    let targetButton = document.querySelector(`a[aria-label="百科"]`);
-                    if (!targetButton) {
-                        document.querySelector(`.css-1ljhlhi`).click();
-                        targetButton = document.querySelector(`a[aria-label="百科"]`);
-                    }
-                    targetButton.click();
-                };
-                a2.classList.add("script-nav-2");
-                selectedElems[0].parentNode.insertBefore(a2, selectedElems[0].nextSibling);
+                    let a3 = document.createElement("a");
+                    let linkText3 = document.createTextNode("[笔记]");
+                    a3.appendChild(linkText3);
+                    a3.onclick = () => {
+                        handleNotes();
+                    };
+                    a3.classList.add("script-nav-3");
+                    selectedElem.parentNode.insertBefore(a3, selectedElem.nextSibling);
+                }, 500);
             }
         };
         let timer = setInterval(checkElementExist, 100);
     }
 
-    // 交易所 高亮5件最近访问物品
-    const recentExchangeItems = localStorage.getItem("recentlyVisitedExchangeResources");
+    function handleNotes() {
+        if (!notesElement) {
+            const notesString = localStorage.getItem("script_notes");
+            notesElement = document.createElement("textarea");
+            notesElement.classList.add("script_notes");
+            notesElement.setAttribute("cols", "40");
+            notesElement.setAttribute("rows", "10");
+            notesElement.setAttribute("placeholder", "笔记内容本地保存，重要信息请另行备份");
+            notesElement.style.display = "block";
+            notesElement.innerHTML = notesString;
+            notesElement.addEventListener(
+                "input",
+                () => {
+                    localStorage.setItem("script_notes", notesElement.value);
+                },
+                false
+            );
+            document.body.appendChild(notesElement);
+        } else if (notesElement.style.display === "none") {
+            notesElement.style.display = "block";
+        } else {
+            notesElement.style.display = "none";
+        }
+    }
 })();
